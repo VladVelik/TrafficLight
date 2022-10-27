bd5dbb1072383adbdc64e83c15b5440271912754
VladVelik <sosinvladik@yandex.ru>
2022-10-27 16:06:15 +0300

    Done

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,15 +7,34 @@
 
 import UIKit
 
+extension UIColor {
+   convenience init(_ red: Int, _ green: Int, _ blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+}
+
 class ViewController: UIViewController {
     @IBOutlet var greenElement: UIView!
     @IBOutlet var yellowElement: UIView!
     @IBOutlet var redElement: UIView!
     @IBOutlet var startButton: UIButton!
     
+    private var currentElement: UIColor = .red
+    
+    private let colors = [UIColor(128, 40, 40), UIColor(128, 128, 0), UIColor(0, 128, 0)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         createView()
+    }
+    
+    @IBAction func startButtonPressed(_ sender: Any) {
+        startButton.setTitle("Next", for: .normal)
+        changeLights()
     }
     
     private func createView() {
@@ -26,13 +45,22 @@ class ViewController: UIViewController {
         redElement.layer.cornerRadius = 100
     }
     
-    @IBAction func startButtonPressed(_ sender: Any) {
-        startButton.setTitle("Next", for: .normal)
-        changeLights()
-    }
-    
     private func changeLights() {
-        
+        switch currentElement {
+        case .red:
+            currentElement = UIColor.yellow
+            redElement.backgroundColor = .red
+            greenElement.backgroundColor = colors[2]
+        case .yellow:
+            currentElement = UIColor.green
+            yellowElement.backgroundColor = .yellow
+            redElement.backgroundColor = colors[0]
+        case .green:
+            currentElement = UIColor.red
+            greenElement.backgroundColor = .green
+            yellowElement.backgroundColor = colors[1]
+        default:
+            currentElement = UIColor.red
+        }
     }
 }
-
