10f215d19b1674b0bef419740483ca1e10ec1764
VladVelik <sosinvladik@yandex.ru>
2022-10-27 14:35:45 +0300

    Traffic light

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var greenElement: UIView!
+    @IBOutlet var yellowElement: UIView!
+    @IBOutlet var redElement: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createView()
     }
+    
+    private func createView() {
+        startButton.layer.cornerRadius = 10
 
-
+        greenElement.layer.cornerRadius = 100
+        yellowElement.layer.cornerRadius = 100
+        redElement.layer.cornerRadius = 100
+    }
+    
+    @IBAction func startButtonPressed(_ sender: Any) {
+        startButton.setTitle("Next", for: .normal)
+        changeLights()
+    }
+    
+    private func changeLights() {
+        
+    }
 }
 
